d728fe5a88e53d6683cd5943d342479dab398f95
DESKTOP-6OAQMU9\mis <emtyariwa@gmail.com>
2021-06-25 17:03:09 +0700

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'add-book' },
+  { path: 'books-list', component : BookListComponent },
+  { path: 'add-book' ,component : AddBookComponent },
+  { path: 'edit-book/:id' ,component : BookDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
